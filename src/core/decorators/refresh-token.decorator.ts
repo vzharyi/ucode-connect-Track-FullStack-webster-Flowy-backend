d472a90d1e426f30e21d89aa52a49621800e650b
vzharyi <119208171+vzharyi@users.noreply.github.com>
2025-11-30 15:05:07 +0200

    Update refresh-token.decorator.ts

--- src/core/decorators/refresh-token.decorator.ts
+++ src/core/decorators/refresh-token.decorator.ts
@@ -2,16 +2,22 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { RefreshTokenPayloadType } from '../types/request.types';
 
 export const RefreshToken = createParamDecorator(
-  (data: keyof RefreshTokenPayloadType | undefined, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    
-    if (!request.user) return null;
+    (data: keyof RefreshTokenPayloadType | undefined, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+        if (!request.user) return null;
+        
 
-    const { userId, ...payload } = request.user;
-    const refreshTokenPayload: RefreshTokenPayloadType = payload;
+        const { userId, ...payload } = request.user;
+        const refreshTokenPayload: RefreshTokenPayloadType = payload;
 
-    return data ? refreshTokenPayload?.[data] : refreshTokenPayload;
-  },
+        return data ? refreshTokenPayload?.[data] : refreshTokenPayload;
+    },
 );
 
-export type RefreshTokenPayload = RefreshTokenPayloadType;
+
+export class RefreshTokenPayload {
+    expiresIn: number;
+    createdAt: number;
+    nonce: string;
+
+}
